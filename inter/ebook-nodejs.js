7891edc2049a352f4a0d9f8d45f9613ee3a3317d
BADMalik <makikzaman133@gmail.com>
2023-07-17 11:43:48 +0500

    node js ebook

--- inter/ebook-nodejs.js
+++ inter/ebook-nodejs.js
@@ -1,26 +1,25 @@
+var async = require("async");
 function shortTimeFunction(callback) {
-    setTimeout(function() {
-    callback(null, 'resultOfShortTime');
-    }, 200);
-   }
-   function mediumTimeFunction(callback) {
-    setTimeout(function() {
-    callback(null, 'resultOfMediumTime');
-    }, 500);
-   }
-   function longTimeFunction(callback) {
-    setTimeout(function() {
-    callback(null, 'resultOfLongTime');
-    }, 1000);
-   }
-   async.parallel([
-    shortTimeFunction,
-   https://riptutorial.com/ 25
-    mediumTimeFunction,
-    longTimeFunction
-    ],
-    function(err, results) {
+  setTimeout(function () {
+    callback(null, "resultOfShortTime");
+  }, 200);
+}
+function mediumTimeFunction(callback) {
+  setTimeout(function () {
+    callback(null, "resultOfMediumTime");
+  }, 500);
+}
+function longTimeFunction(callback) {
+  setTimeout(function () {
+    callback(null, "resultOfLongTime");
+  }, 1000);
+}
+async.parallel(
+  [shortTimeFunction, mediumTimeFunction, longTimeFunction],
+  function (err, results) {
     if (err) {
-    return console.error(err);
+      return console.error(err);
     }
-    conso
+    console.log(results);
+  }
+);
