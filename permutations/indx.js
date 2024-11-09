a34375f39a673d616c80ddc8a4d556d62e28a519
“Bilal” <malikzaman133@gmail.com>
2024-11-10 01:38:01 +0500

    Push

--- permutations/indx.js
+++ permutations/indx.js
@@ -6,15 +6,28 @@ var permute = function (nums) {
   const result = [];
 
   const backtrack = (current, remaining) => {
+    console.log("NEW ->->->->->->->", { remaining, current });
     if (remaining.length === 0) {
+      console.log({ slice: current.slice() });
       result.push(current.slice());
       return;
     }
+    console.log(
+      "================================================================ "
+    );
     for (let i = 0; i < remaining.length; i++) {
       current.push(remaining[i]);
+      console.log({ remaining });
       let firstHalf = remaining.slice(0, i);
       let secondHalf = remaining.slice(i + 1);
-      backtrack(current, firstHalf.concat(secondHalf));      
+      console.log({ current, firstHalf, secondHalf, i });
+      backtrack(current, firstHalf.concat(secondHalf));
+      console.log("++++++++++++++++++++++++++++++++++++++++++++++++", {
+        i,
+        current,
+        remaining,
+        result,
+      });
       current.pop();
     }
   };
