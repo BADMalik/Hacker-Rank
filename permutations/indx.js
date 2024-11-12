122df402440212404a2b4e09d5aac31f518f6c17
“Bilal” <malikzaman133@gmail.com>
2024-11-13 01:30:58 +0500

    PUSH

--- permutations/indx.js
+++ permutations/indx.js
@@ -4,7 +4,7 @@ var dfspermute = function (nums) {
   const res = [];
   const visited = [];
 
-  const dfs = (subset) => {
+  const dfs = (subset, level) => {
     // console.log({ subset });
     if (subset.length === nums.length) {
       console.log({ nums, visited, subset }, "reaced \n");
@@ -12,19 +12,19 @@ var dfspermute = function (nums) {
       return;
     }
     for (let i = 0; i < nums.length; i++) {
-      console.log({ nums, visited, subset, i }, "\n");
+      console.log({ level, visited, subset, i }, "\n");
       if (!visited[i]) {
         // Pick the choice
 
-        console.log({ nums, visited, subset, i }, "initial\n");
+        console.log({ level, visited, subset, i }, "initial\n");
         visited[i] = true;
         subset.push(nums[i]);
-        console.log({ visited, subset, i }, "before\n");
+        console.log({ level, visited, subset, i }, "before\n");
 
         // console.log({ subset, nums });
-        dfs(subset);
+        dfs(subset, level + 1);
 
-        console.log({ visited, subset, i }, "after\n");
+        console.log({ level, visited, subset, i }, "after\n");
         // Undo the choice
         visited[i] = false;
         subset.pop();
@@ -32,7 +32,7 @@ var dfspermute = function (nums) {
     }
   };
 
-  dfs([]);
+  dfs([], 0);
   return res;
 };
 
