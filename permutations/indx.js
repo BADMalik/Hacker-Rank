618a8eef2f60b504d9508a6a7c4f9ee7874177ba
“Bilal” <malikzaman133@gmail.com>
2024-11-13 01:35:48 +0500

    PUSH

--- permutations/indx.js
+++ permutations/indx.js
@@ -1,11 +1,9 @@
-// console.log(permute([1, 2, 3]));
 
 var dfspermute = function (nums) {
   const res = [];
   const visited = [];
 
   const dfs = (subset, level) => {
-    // console.log({ subset });
     if (subset.length === nums.length) {
       console.log({ nums, visited, subset }, "reaced \n");
       res.push([...subset]); // Make a copy of subset
@@ -16,17 +14,14 @@ var dfspermute = function (nums) {
       if (!visited[i]) {
         // Pick the choice
 
-        
         console.log({ level, visited, subset, i }, "initial\n");
         visited[i] = true;
         subset.push(nums[i]);
         console.log({ level, visited, subset, i }, "before\n");
 
-        // console.log({ subset, nums });
         dfs(subset, level + 1);
 
         console.log({ level, visited, subset, i }, "after\n");
-        // Undo the choice
         visited[i] = false;
         subset.pop();
       }
